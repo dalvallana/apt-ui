e6c5c1f3ebc3e957e93361f6b65207caa2ecaba1
dalvallana <daao87@gmail.com>
2021-05-19 15:26:31 +0200

    Add fontawesome vue packages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,45 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import { library } from '@fortawesome/fontawesome-svg-core';
+// internal icons
+import {
+  faCheck,
+  faCheckCircle,
+  faInfoCircle,
+  faExclamationTriangle,
+  faExclamationCircle,
+  faArrowUp,
+  faAngleRight,
+  faAngleLeft,
+  faAngleDown,
+  faEye,
+  faEyeSlash,
+  faCaretDown,
+  faCaretUp,
+  faUpload,
+  faLink,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Buefy from 'buefy';
 import DefaultLayout from '@/layouts/Default';
+
+library.add(
+  faCheck,
+  faCheckCircle,
+  faInfoCircle,
+  faExclamationTriangle,
+  faExclamationCircle,
+  faArrowUp,
+  faAngleRight,
+  faAngleLeft,
+  faAngleDown,
+  faEye,
+  faEyeSlash,
+  faCaretDown,
+  faCaretUp,
+  faUpload,
+  faLink
+);
 
 export default (Vue, { head }) => {
   head.link.push({
@@ -9,7 +47,11 @@ export default (Vue, { head }) => {
     href:
       'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;0,700;1,400;1,700&family=Newsreader:ital,wght@0,400;0,700;1,400;1,700',
   });
+  Vue.component('vue-fontawesome', FontAwesomeIcon);
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
-  Vue.use(Buefy);
+  Vue.use(Buefy, {
+    defaultIconComponent: 'vue-fontawesome',
+    defaultIconPack: 'fas',
+  });
 };
